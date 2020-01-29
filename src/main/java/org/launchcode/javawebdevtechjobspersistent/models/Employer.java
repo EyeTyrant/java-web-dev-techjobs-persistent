61a9af2e7d4e26f277c2ee426da3745c8c6f05e7
EyeTyrant <1414jg@gmail.com>
2020-01-29 12:36:28 -0600

    part2_models complete

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,26 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+  @NotBlank(message = "Must enter a location")
+  private String location;
 
+  public Employer() {}
+
+  public Employer(String location) {
+    this.location = location;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
 }
